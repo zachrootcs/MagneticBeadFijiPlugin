1fbde975e37f4b49207ac77fcbe3a55c740fd561
Zach <zach.root111@gmail.com>
2024-07-18 17:45:38 -0400

    Finished ZLUT Fit and added support for avi

--- src/main/java/com/zachRoot/MagneticBead.java
+++ src/main/java/com/zachRoot/MagneticBead.java
@@ -2,8 +2,12 @@ package com.zachRoot;
 
 import java.awt.Color;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +21,7 @@ import ij.gui.Overlay;
 import ij.gui.Roi;
 import ij.io.FileSaver;
 import ij.measure.CurveFitter;
+import ij.plugin.AVI_Reader;
 import ij.plugin.filter.PlugInFilter;
 
 import ij.process.FloatProcessor;
@@ -34,6 +39,7 @@ public class MagneticBead implements PlugInFilter {
 	private int height;
 	private int radius;
 	
+	//To do implement
 	private String length_unit;
 	
 	private int centerRoiWidth = 10;
@@ -70,11 +76,19 @@ public class MagneticBead implements PlugInFilter {
 		processStack(image.getStack());
 		
 	}
-
+	
 	private void createZLut() {
+		
 		
 		// Directory for ZLUT Reference Images
 		String directory_path = getDirectoryFromUser();		
+		
+		
+		if(isZlutInFolder(directory_path)) {
+			System.out.println("ZlutSaved");
+			//already saves
+			return;
+		}
 		
 		// ZLut Component Images
 		LinkedList<ImagePlus> images = getReferenceImages(directory_path);
@@ -100,34 +114,65 @@ public class MagneticBead implements PlugInFilter {
 			
 			// Get the height of the image from the image title
 			zlutHeights[i] = extractStartingNumber(images.get(i).getTitle());
-			//ADD A SAVING ZLUT FEATURE
-			
-			//System.out.println(Arrays.toString(zlutHeights));
+
 		}
-		//Prompt user to save zlut
-		promptUserToSaveZLut(directory_path); 
-			
 		
+		promptUserToSaveZLut(directory_path); 
 	}
-
-
-
-
 	
+	
+	public void processStack(ImageStack stack) {
+		//Start at one and process each frame in stack
+		for(int i = 1; i<=stack.size(); i++ ) {
+			processIP(stack.getProcessor(i), i);
+			
+			
+			// REMOVE RETURN ONLY FOR TESTING ONE FRAME
+			return;
+		}
+	}
+	
+	// directory_path is the default go to
 	private void promptUserToSaveZLut(String directory_path) {
 		
 		GenericDialog g = new GenericDialog("Save ZLUT?");
 		g.addMessage("Select a folder to save the ZLUT to");
 		g.addDirectoryField("Directory", directory_path);
 		g.showDialog();
+		
+		if(g.wasCanceled()) {
+			return;
+		}
+		
 		directory_path = g.getNextString();
 		
+		//Save ZLUT
 		FileSaver f = new FileSaver(new ImagePlus("ZLUT", new FloatProcessor(zlut)));
 		f.saveAsTiff(directory_path + "ZLUT.tif");
+		
+		//Save Heights
+		String zlutHeightsPath = directory_path + "zlutHeights.csv";
+		File zlutHeightsfile = new File(zlutHeightsPath); 
+		
+		try {
+			FileWriter outputfile = new FileWriter(zlutHeightsfile); 
+			String output = "";
+			
+			for(int i: zlutHeights) {
+				output += i + ",";
+			}
+			
+			outputfile.write(output);
+			outputfile.close();
+		}
+		catch(IOException e){
+			IJ.showMessage("ZLUT could not be saved");
+		}
 		
 		return;
 	}
 
+	
 	public int[] getCenterOfMass(ImageProcessor ip) {
 		int[] xyCord = null;
 		//TO DO CHECK IF THIS MAKES SENSE?
@@ -145,6 +190,7 @@ public class MagneticBead implements PlugInFilter {
 	}
 	
 
+	
 	public void processIP(ImageProcessor ip, int slice) {
 		
 		// X,Y COM Center Coordinates
@@ -154,7 +200,7 @@ public class MagneticBead implements PlugInFilter {
 		double[] xyCordSubPixel = xyRowColConvolve(ip, xyCord[0], xyCord[1]);
 		
 		//Optional May Remove
-		addPointToOverlay(xyCordSubPixel,slice);
+		addPointToOverlay(xyCordSubPixel, slice);
 		
 		float[] radialProfile = createRadialProfile(xyCordSubPixel, ip);
 		
@@ -165,51 +211,58 @@ public class MagneticBead implements PlugInFilter {
 	
 	
 	private double compareWithZLut(float[] radialProfile) {
-		//new ImagePlus("",new FloatProcessor(zlut)).show();
 		
+		// A copy to perform operations on. Functionally the same
+		float[][] zlutC = new float[zlut.length][zlut[0].length];
+				
 		//add something for normalization
-		//add something to make sure zlut is saved beforehand
 		for(int i = 0; i<zlut.length; i++) {
+			
 			// Find difference between absolute difference between radial profile and zlut[i] signal and index j
-			for(int j = 0; j<zlut[0].length; j++) {
-				zlut[i][j] = Math.abs(radialProfile[j]-zlut[i][j]);
+			for(int j = 0; j < zlut[0].length; j++) {
+				//Copy is filled here
+				zlutC[i][j] = Math.abs(radialProfile[j]-zlut[i][j]);
 			}
-			//new ImagePlus("",new FloatProcessor(zlut)).show();
+			
 			//sum that difference and add that to the first column
 			float sum = 0f;
-			for(int j = 0; j<zlut[0].length; j++) {
-				sum += zlut[i][j];
+			for(int j = 0; j<zlutC[0].length; j++) {
+				sum += zlutC[i][j];
 			}
-			zlut[i][0] = sum;
-		}
-		// Find minimum
-		int minimumDifferenceIndex = 0;
-		for(int i = 0; i < zlut.length; i++) {
-			if (zlut[i][0] < zlut[minimumDifferenceIndex][0]) {
-				System.out.println("zlut " + zlut[i][0]);
-				minimumDifferenceIndex = i;
-			}
-			System.out.println("zlut " + zlut[i][0]);
 			
+			zlutC[i][0] = sum;
 		}
 		
-		//System.out.println(zlutHeights[minimumDifferenceIndex]);
+		// Find minimum
+		int minDiffIndex = 0;
+		for(int i = 0; i < zlutC.length; i++) {
+			if (zlutC[i][0] < zlutC[minDiffIndex][0]) {
+				minDiffIndex = i;
+			}
+		}
+		
 		
 		//5 point curve fit
 		//Make sure it stays in bounds
-		int leftBound = (minimumDifferenceIndex-2 < 0) ? 0 : minimumDifferenceIndex-3;
-		int RightBound = (minimumDifferenceIndex+2 > zlut[0].length-1) ? zlut[0].length-1 : minimumDifferenceIndex+3;
+		/*
+		if(minDiffIndex-2 < 0 || minDiffIndex+2 > zlutC[0].length-1) {
+			IJ.showMessage("The radial profile of the image was too closely matched to a radial profile on the edge of the ZLUT. This causes the fit to be inacurate as it doesn't have enough data to create a 5 point quadratic fit. Program Quitting. ");
+			return Double.NaN;
+		}
+		*/
+		int leftBound = (minDiffIndex-2 < 0) ? 0 : minDiffIndex-2;
+		int RightBound = (minDiffIndex+2 > zlutC.length-1) ? zlutC.length-1 : minDiffIndex+2;
 		
 		System.out.println(leftBound + " " + RightBound);
-		System.out.println(minimumDifferenceIndex);
-				
-		double[] data    = new double[RightBound-leftBound];
-		double[] indexes = new double[RightBound-leftBound];
+		System.out.println(minDiffIndex);
+		System.out.println(zlutC.length);
+		double[] data    = new double[RightBound-leftBound+1];
+		double[] indexes = new double[RightBound-leftBound+1];
 		
 		
-		for(int i = 0; i < RightBound-leftBound; i++) {
+		for(int i = 0; i < RightBound-leftBound+1; i++) {
 			indexes[i] = zlutHeights[i];
-			data[i] = zlut[i][0];
+			data[i] = zlutC[i][0];
 		}
 		
 		System.out.println("Indexes " + Arrays.toString(indexes));
@@ -218,13 +271,18 @@ public class MagneticBead implements PlugInFilter {
 		
 		CurveFitter leastdiferenceFit = new CurveFitter(indexes, data);
 		leastdiferenceFit.doCustomFit("y = a + b*x + c*c*x*x", new double[] {0,0,0}, false);
-		System.out.println(leastdiferenceFit.getNumParams("y = a + b*x + c*c*x*x"));
+		
 		System.out.println(leastdiferenceFit.getStatusString());
 		System.out.println(Arrays.toString(leastdiferenceFit.getParams()));
 		
+		// -b/2c^2 because c^2 is the variable fit in the equation
 		return -leastdiferenceFit.getParams()[1]/(2*leastdiferenceFit.getParams()[2]*leastdiferenceFit.getParams()[2]);
 	}
 
+	
+	
+	
+	
 	private float[] createRadialProfile(double[] xyCord, ImageProcessor ip) {
 		
 		//ip.getOverlay().add(new OvalRoi(xyCord[0], xyCord[1], radius, radius));
@@ -346,9 +404,9 @@ public class MagneticBead implements PlugInFilter {
 		
 		// makes sure to not go out of bounds
 		int xleftBound = (xMaxIndex-3 < 0) ? 0 : xMaxIndex-3;
-		int xRightBound = (xMaxIndex+3 > width) ? width : xMaxIndex+3;
+		int xRightBound = (xMaxIndex+3 > width-1) ? width-1 : xMaxIndex+3;
 		int yleftBound = (yMaxIndex-3 < 0) ? 0 : yMaxIndex-3;
-		int yRightBound = (yMaxIndex+3 > width) ? width : yMaxIndex+3;
+		int yRightBound = (yMaxIndex+3 > width-1) ? width-1 : yMaxIndex+3;
 		
 		double[] xData = new double[7];
 		double[] xIndexes = new double[7];
@@ -390,17 +448,6 @@ public class MagneticBead implements PlugInFilter {
 
 	
 
-	public void processStack(ImageStack stack) {
-		//Start at one and process each frame in stack
-		for(int i = 1; i<=stack.size(); i++ ) {
-			processIP(stack.getProcessor(i), i);
-			
-			
-			// REMOVE RETURN ONLY FOR TESTING ONE FRAME
-			return;
-		}
-	}
-	
 	
 
 	/* This function is a java version of this matlab code:
@@ -517,11 +564,58 @@ public class MagneticBead implements PlugInFilter {
 		return g.getNextString();
 		
 	}
+	
 	private LinkedList<ImagePlus> getReferenceImages(String directory_path) {
 		
+		AVI_Reader aviRead = new AVI_Reader();
+		
+		File parent_directory = new File(directory_path);
+		File[] directory = parent_directory.listFiles();
+		
+		String regex = "^\\d+[A-Za-z]+\\.[A-Za-z0-9]+$";
+        Pattern pattern = Pattern.compile(regex);
+        
+        LinkedList<ImagePlus> images = new LinkedList<>();
+        for (File file: directory) {
+        	
+        	if(file.isDirectory()) {
+        		images.addAll(getReferenceImages(file.getAbsolutePath()));
+        	}
+        	
+            Matcher matcher = pattern.matcher(file.getName());
+            
+            if (matcher.matches()) {
+                images.add(new ImagePlus(file.getAbsolutePath()));
+                
+            } else if(file.getName().endsWith(".avi")){
+            	
+            	//Dumb way to get all the images 
+            	//Forced to becasue of how james organized the data
+            	//Convert to imageProcessor 
+            	ImageProcessor img = aviRead.makeStack(file.getAbsolutePath(),1,0,false,false,false).getProcessor(1);
+            	//Name the image with the proper name
+            	images.add(new ImagePlus(parent_directory.getName(),img));
+            	
+            }
+                
+        }
+        
+        return images;
+        
+        
+		/*
+		 * 	;
+		 * file.getName().endsWith(".avi")
+		for(File subDir: directory) {
+			if(subDir.isDirectory()) {
+				for(File file: subDir.listFiles()) {
+					if(file.getName().endsWith(".avi"))
+				}
+			}
+		}
+		// of the form: numbers letters . extension
 		File[] directory = new File(directory_path).listFiles();
 		
-		// of the form: numbers letters . extension
 		String regex = "^\\d+[A-Za-z]+\\.[A-Za-z0-9]+$";
         Pattern pattern = Pattern.compile(regex);
         
@@ -530,8 +624,7 @@ public class MagneticBead implements PlugInFilter {
         	
             Matcher matcher = pattern.matcher(file.getName());
             
-            if (matcher.matches()) {
-            	
+            if (matcher.matches() ) {
                 System.out.println(file.getName() + " matches the pattern.");
                 images.add(new ImagePlus(file.getAbsolutePath()));
                 
@@ -540,9 +633,73 @@ public class MagneticBead implements PlugInFilter {
             }
         }
         return images;
+        
+        */
 	}
+
 	
+	//Checks if zlut is in the folder and calls the loading function if found
+	private boolean isZlutInFolder(String directory_path) {
+				
+		if(!new File(directory_path, "ZLUT.tif").exists()) {
+			return false;
+		}
+		if(!new File(directory_path, "zlutHeights.csv").exists()) {
+			return false;
+		}
+		
+		loadZlut(new File(directory_path, "ZLUT.tif"));
+		loadZLUTHeights(new File(directory_path, "zlutHeights.csv"));
+		
+		return true;
+	}
+
+	// Loads private height variable from integers in file
+	private void loadZLUTHeights(File file) {
+		
+		// Prealloc
+		zlutHeights = new int[zlut.length];
+		try {
+			//File is program made and follows the csv format
+			Scanner scanner = new Scanner(file);
+			
+			if(scanner.hasNext()) {
+				//Split along commas 
+				String line = scanner.nextLine();
+				String[] split = line.split(",");
+				
+				// Parse to int
+				for(int i = 0; i<zlut.length; i++) {
+					zlutHeights[i] = Integer.parseInt(split[i]);
+				}
+			}
+			scanner.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	
+	// Loads private zlut variable with pixels from File
+	private void loadZlut(File zlutFile) {
+		
+		FloatProcessor ip = (new ImagePlus(zlutFile.getAbsolutePath())).getProcessor().convertToFloatProcessor();
+      
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        
+        float[] pixels = (float[]) ip.getPixels();
+        
+        zlut = new float[width][height];
+       
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+            	zlut[x][y] = pixels[y*width + x];
+            }
+        }
+	}
+
 	// Title of image should be numbers units . extension
 	// Returns starting numbers
 	private int extractStartingNumber(String title) {
@@ -584,8 +741,8 @@ public class MagneticBead implements PlugInFilter {
 		
 		// start ImageJ
 		new ImageJ();
-		
-		ImagePlus image = new ImagePlus("C:\\Users\\7060 Yoder3\\Desktop\\MagneticBeadProject\\videos\\croped3.tif");
+//C:\\Users\\7060 Yoder3\\Desktop\\MagneticBeadProject\\videos\\croped3.tif
+		ImagePlus image = new ImagePlus("C:\\Users\\7060 Yoder3\\Desktop\\MagneticBeadProject\\07-16-24 Data\\Bead 1 ZLUT 1 50-55\\54870\\2024-07-16-48862837.avi");
 		image.show();
 		
 		// run the plugin
